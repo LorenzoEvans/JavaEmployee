a944d318a998424963fff49c1edd67d2f43c622b
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-14 20:24:08 -0500

    tweaking main fn

---
+++ src/employeeApp/Main.java
@@ -0,0 +1,9 @@
+package employeeApp;
+
+public class Main {
+    public static void main(String[] args) {
+        HealthPlan h1 = new HealthPlan("My Health Plan");
+        HealthPlan h2 = new HealthPlan("My other Health Plan");
+        System.out.println(h1);
+    }
+}
